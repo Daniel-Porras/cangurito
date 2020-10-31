23695c4322832323ad9492998254b46d4d1a476a
Daniel-Porras <dporrasa@unal.edu.co>
2020-10-31 18:37:29 -0500

    Icono de la app

--- app/src/main/java/com/example/canguro/MainActivity.kt
+++ app/src/main/java/com/example/canguro/MainActivity.kt
@@ -7,14 +7,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (4==4 && 5!=5){
-            println("Hola como estas")
-        }
-        //hola como estas
-        var hola="hola como estas"
-        println("Esto lo hice en cangurito")
-        val holacc="sera que si sirve a distancia?"
-        // y desde el otro lado sirve?
-
     }
 }
